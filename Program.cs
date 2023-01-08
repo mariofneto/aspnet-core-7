462f39acf77e565168c4b353777aa6ca4d3f5180
Mário Ferreira <marioo.netoo3@gmail.com>
2023-01-08 08:43:40 -0300

    async e await em request e reponse

--- Program.cs
+++ Program.cs
@@ -3,9 +3,15 @@ var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
 
-app.Run(async (HttpContext context)=>{
-    await context.Response.WriteAsync("Fala rapaziada");
-    await context.Response .WriteAsync("Tudo bem?");
+app.Run(async (HttpContext context) =>
+{
+    // string method = context.Request.Method;
+    var agente = context.Request.Headers.UserAgent;
+    await context.Response.WriteAsync("<h1>Fala rapaziada</h1>");
+    await context.Response.WriteAsync("<h2>tudo bem?</h2>");
+    await context.Response.WriteAsync($"<h2>agente: {agente}</h2>");
+    await context.Response.WriteAsync($"<h2>hora do momento e {DateTime.Now}</h2>");
+    // await context.Response.WriteAsync($"<h3>{method}</h3>");
 });
 
 app.Run();
