65b1404f7c50414031e59eff544d2f1f778906df
Mário Ferreira <marioo.netoo3@gmail.com>
2023-01-16 10:00:25 -0300

    projeto razor criado

--- S01_Introduction/Controllers/CategoryController.cs
+++ S01_Introduction/Controllers/CategoryController.cs
@@ -53,5 +53,41 @@ namespace S01_Introduction.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+                return NotFound();
+                
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            var search = _db.Categories.FirstOrDefault(x => x.Name == obj.Name);
+            if (obj.Name == search.Name.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The Name is exists");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
     }
 }
